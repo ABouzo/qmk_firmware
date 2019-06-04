61525c00c756075fcfe589aaabf46fb704ff189c
Angel Bouzo <angel.bouzo@comcast.com>
2019-06-04 15:07:21 -0700

    added a stupid keyboard layout for one side of lets split

---
+++ keyboards/lets_split/keymaps/right_only/config.h
@@ -0,0 +1,15 @@
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 6
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 17
+#define RGBLIGHT_VAL_STEP 17
+
+#define CONFIG_USER_H
+
+#include "../../config.h"
+
+/* Select hand configuration */
+// #define MASTER_LEFT
+#define MASTER_RIGHT
+// #define EE_HANDS
